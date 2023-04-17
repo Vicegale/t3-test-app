26e4870fe150aa5799313f01dae727cbb34f2955
vicegale <vicegale@vicegale.com>
2023-04-17 23:46:21 +0100

    Animations

--- src/pages/people.tsx
+++ src/pages/people.tsx
@@ -1,7 +1,8 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import autoAnimate from '@formkit/auto-animate'
 
 
 import { api } from "~/utils/api";
@@ -9,11 +10,19 @@ import { api } from "~/utils/api";
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const { data } = api.person.getAll.useQuery();
-
+  
   const [selectedId, setSelectedId] = useState("");
 
   const things = api.person.getThings.useQuery({ id: selectedId });
 
+  //auto-animate
+  const parent = useRef(null);
+  useEffect(() => {
+    parent.current && autoAnimate(parent.current)
+  }, [parent])
+
+  //Pong!
+  const [reveal, setReveal] = useState(false);
 
   return (
     <>
@@ -27,6 +36,10 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
+          <div ref={parent} className="flex flex-row">
+            <div onClick={() => setReveal(!reveal)} className="text-white border-2 border-white p-4 rounded-full hover:bg-purple-400 hover:text-white transition-colors duration">Ping!</div>
+            {reveal ? <div onClick={() => setReveal(!reveal)} className="w-fit border-2 border-white text-white p-4 rounded-full hover:bg-white hover:text-black transition-colors duration">Pong!</div> : <></>}
+          </div>
           <div className="grid grid-cols-2">
             <div className="flex flex-col gap-4 sm:grid-cols-2 md:gap-8 text-white">
               {data ? data?.map((person) => (<div className="border-2 border-white p-4 rounded-full hover:bg-white hover:text-black transition-colors duration" key={person.id} onClick={() => setSelectedId(person.id)}>{person.name} - {person.age} years old</div>)): "Loading people"}
