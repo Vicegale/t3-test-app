7c21cf5c769a9f99ea8a51fc6572cb0da84d045d
vicegale <vicegale@vicegale.com>
2023-04-17 02:06:16 +0100

    Button to people page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,6 +43,16 @@ const Home: NextPage = () => {
                 to deploy it.
               </div>
             </Link>
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="/people"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">People →</h3>
+              <div className="text-lg">
+                Go to the /people page
+              </div>
+            </Link>
           </div>
           <p className="text-2xl text-white">
             {hello.data ? hello.data.greeting : "Loading tRPC query..."}
