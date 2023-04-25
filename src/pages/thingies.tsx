01a12fd0d0d71bea37597f267e6e7d8363bc6f51
vicegale <vicegale@vicegale.com>
2023-04-25 13:06:25 +0100

    whitespace

--- src/pages/thingies.tsx
+++ src/pages/thingies.tsx
@@ -3,10 +3,10 @@ import Tile from "~/components/ui/griddy/tile";
 
 const Thingies: NextPage = ()  => {
     return(
-    <div className="grid grid-cols-10 gap-0 bg-black">
-    {Array.from(Array(1000).keys()).map((_, i) => <Tile key={i} />)}
-    </div>
-    )   
+            <div className="grid grid-cols-10 gap-0 bg-black">
+            {Array.from(Array(1000).keys()).map((_, i) => <Tile key={i} />)}
+            </div>
+          )   
 }
 
 export default Thingies;
